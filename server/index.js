8f5199b9ae7c25a04fdcaf72652b47e8de434721
Amina Nisic <amina.nisic@stu.ibu.edu.ba>
2023-03-30 01:25:02 +0200

    Users table

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
+const db = require('./models')
+
 app.use(express.json());
 
-app.listen(3001, () => {
-    console.log("Server running on port 3001");
-});
+db.sequelize.sync().then(()=> {
+    app.listen(3001, () => {
+        console.log("Server running on port 3001");
+    });
+})
+   
 
---
+++ server/models/Users.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const Users = sequelize.define("Users", {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }    
+    })
+
+    return Users;
+}
