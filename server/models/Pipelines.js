cd43987d962429225dc28d7ab120270eeba3bd4d
Amina Nisic <amina.nisic@stu.ibu.edu.ba>
2023-05-15 00:21:48 +0200

    pajplans

---
+++ server/models/Pipelines.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+
+    const Pipelines = sequelize.define("Pipelines", {
+        pipelineName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        language: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastBuild: {
+            type: DataTypes.DATE,
+            allowNull: false
+        } ,
+        actions: {
+            type: DataTypes.STRING(5000),
+            allowNull: false
+        },
+        stateOfPipeline: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }      
+    })
+
+    return Pipelines;
+}
