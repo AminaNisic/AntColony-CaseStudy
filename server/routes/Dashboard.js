7ff5fffd4c6c12b0e68267fac09a765220e84ab4
Amina Nisic <amina.nisic@stu.ibu.edu.ba>
2023-05-18 21:52:16 +0200

    bejzik rauts

--- server/routes/Dashboard.js
+++ server/routes/Dashboard.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const { Pipelines } = require("../models");
 const { validateToken } = require("../middleware/AuthMiddleware");
 
+//test route for jwt
 //validateToken
-router.get("/", validateToken, async (req,res) => {
+/*router.get("/", validateToken, async (req,res) => {
     return res.json("dashboard");
+});*/
+
+//baseroute, will user map tomorrow
+router.get("/", async (req, res) => {
+    const listOfPipelines = await Pipelines.findAll();
+    res.json(listOfPipelines);
+  });
+
+  //probably treba i ovo ako user hoce da previewa pajplajn
+router.get("/ById/:id", async (req, res) => {
+  const id=req.params.id
+  const Pipeline = await Pipelines.findByPk(id);
+  res.json(Pipeline);
+});
+
+//kriejt pajplajn
+router.post("/createPipeline", async (req, res) => {
+  const pipeline = req.body;
+  await Pipelines.create(pipeline);
+  res.json(pipeline);
 });
 
 module.exports = router;
