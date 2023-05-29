d3af9ecfead7e2c6ef4f8607ec4f01bb174da3f8
selmaku <128999042+selmaku@users.noreply.github.com>
2023-05-29 06:16:09 +0200

    Add files via upload

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Navbar from './pages/Navbar';
-import testcrud from './pages/testcrud';
-import ProjectsList from './pages/ProjectsList';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/register" exact component={Register} />
-        <Route path="/login" exact component={Login} />
-        <Route path="/testcrud" exact component={testcrud} />
-        <Route path="/projectslist" exact component={ProjectsList}/>
-      </Switch>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
