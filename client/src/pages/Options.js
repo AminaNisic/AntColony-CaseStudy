f36adeef9e4525a4b85ba5a6a15b2a81a4338a52
selmaku <128999042+selmaku@users.noreply.github.com>
2023-05-30 04:14:54 +0200

    Add files via upload

--- client/src/pages/Options.js
+++ client/src/pages/Options.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React , { useState } from 'react';
 import '../App.css';
 
 function Options({ x, y, onClose, onEdit, onDelete, onRun }) {
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupMessage, setPopupMessage] = useState('');
+
+
   const handleOptionClick = (option) => {
     if (option === 'edit') {
       onEdit();
@@ -10,6 +14,14 @@ function Options({ x, y, onClose, onEdit, onDelete, onRun }) {
     } else if (option === 'run') {
       onRun();
     }
+  };
+
+  const handleRun = () => {
+    setPopupMessage('Success!');
+    setShowPopup(true);
+    setTimeout(() => {
+      setShowPopup(false);
+    }, 3000); 
   };
 
   return (
@@ -22,6 +34,7 @@ function Options({ x, y, onClose, onEdit, onDelete, onRun }) {
         <li onClick={() => handleOptionClick('delete')}>Delete</li>
         <li onClick={() => handleOptionClick('run')}>Run</li>
       </ul>
+      {showPopup && <div className="popup">{popupMessage}</div>}
     </div>
   );
 }
