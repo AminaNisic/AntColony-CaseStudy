4ccfe9f9a8f754f0af5baaac58fdaa0e176b6c91
selmaku <128999042+selmaku@users.noreply.github.com>
2023-07-02 22:00:12 +0200

    Add files via upload

--- client/src/pages/testcrud.js
+++ client/src/pages/testcrud.js
@@ -5,72 +5,67 @@ import axios from 'axios';
 import '../App.css';
 import { useHistory } from 'react-router-dom';
 
-
 function Testcrud() {
-
   let history = useHistory();
 
-  
   const [listOfProjects, setListOfProjects] = useState([]);
 
   useEffect(() => {
     axios.get("http://localhost:3001/projects/myProjects", {
       headers: {
-          accessToken: localStorage.getItem("accessToken"),
+        accessToken: localStorage.getItem("accessToken"),
       }
-  }).then((response) =>{
+    }).then((response) => {
       setListOfProjects(response.data);
     })
   }, []);
 
-  /*const delet = () => {
-    axios.delete(`http://localhost:3001/projects/${value.id}`, {
-      headers: {
-          accessToken: localStorage.getItem("accessToken"),
-      }
-      }).then((response) => {
-      if (response.data.error) { 
-        alert(response.data.error);
-      } else { 
-        alert("deleted :(");
-      }
-    });
-  };*/
-
   return (
-    <div className="projectcontainer" >
-      {listOfProjects.map((value,key) => {
-        return <div className='projectcard' onClick = {() => {
-          history.push(`/testpage/${value.id}`)
-        }}> 
-        <div className='title'> {value.projectName} </div>
-        
-
-        <div className='title'> {value.repoURL} </div>
-        <div className='title'> {value.status} </div>
-        <div className='title'> {value.createdAt} </div>
-        <div className='title'> {value.updatedAt} </div>
-        <h1>Project id: {value.id} </h1>
-        .
-        <button type="button" className="login-button" onClick={() => {
-          axios.delete(`http://localhost:3001/projects/${value.id}`, {
-            headers: {
-                accessToken: localStorage.getItem("accessToken"),
-            }
-            }).then((response) => {
-            if (response.data.error) { 
-              alert(response.data.error);
-            } else { 
-              alert("deleted :(");
-            }
-          });
-        }}>
-          DELETE NASTY
-        </button>
-        </div>
+    <div className="projectcontainer">
+      {listOfProjects.map((value, key) => {
+        return (
+          <div className='projectcard' onClick={() => {
+            history.push(`/testpage/${value.id}`)
+          }}>
+            <div className='project-name'> {value.projectName} </div>
+            <div className='title'>
+            <span>repoURL:</span> {value.repoURL}
+          </div>
+          <div className='title'>
+            <span>Status:</span> {value.status}
+          </div>
+          <div className='title'>
+            <span>Created at:</span> {value.createdAt}
+          </div>
+          <div className='title'>
+            <span>Updated at:</span> {value.updatedAt}
+          </div>
+          <div className='project-id'>
+            <span>Project ID:</span> {value.id}
+          </div>
+            <button
+              type="button"
+              className="delete-project-button"
+              onClick={() => {
+                axios.delete(`http://localhost:3001/projects/${value.id}`, {
+                  headers: {
+                    accessToken: localStorage.getItem("accessToken"),
+                  }
+                }).then((response) => {
+                  if (response.data.error) {
+                    alert(response.data.error);
+                  } else {
+                    alert("deleted :(");
+                  }
+                });
+              }}>
+              Delete Project
+            </button>
+          </div>
+        );
       })}
     </div>
   );
+}
 
-  }
 export default Testcrud;
