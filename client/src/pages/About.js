73770d02ae38d13519e4a89547126a35df443700
selmaku <128999042+selmaku@users.noreply.github.com>
2023-07-02 04:57:55 +0200

    Add files via upload

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,8 +2,6 @@ import React from 'react';
 import '../App.css';
 import { useHistory } from 'react-router-dom';
 
-
-
 const About = () => {
 
     const history = useHistory();
@@ -12,32 +10,27 @@ const About = () => {
         history.push('/features');
       };
 
-
   return (
     <div className="about-container">
-        <div className="about-banner">
-            <div className="about-text-section"> 
-                <p className="about-subheading">
-                    ABOUT US:
-                </p>
-                <h1 className="about-heading">
-                    We are PIPELINER!
-                </h1>
-                <p className="about-text">
-                    We are women in STEM.
-                    <br />
-                    And we want to make pipes fun!
-                    <br />
-                    Learn more about us and our features.
-                </p>
-                <button className="more-button" onClick={handleFeaturesClick}>
-                    Find out more
-                </button>
+        <div className="about-text"> 
+            <p className="about-heading1">
+                ABOUT US:
+            </p>
+            <h1 className="about-heading2">
+                WE ARE PIPELINER!
+            </h1>
+            <p className="about-paragraph">
+                We are women in STEM.
+                <br />
+                And we want to make pipes fun!
+                <br />
+                Learn more about us and our features.
+            </p>
+            <button className="more-button" onClick={handleFeaturesClick}>
+                Find out more
+            </button>
             </div>
         </div>
-  
-
-    </div>
   )
 }
 
