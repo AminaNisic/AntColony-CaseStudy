3f2b3d1d934e7953dcff307fbe91461fa458c9dc
Amina Nisic <amina.nisic@stu.ibu.edu.ba>
2023-07-15 18:57:42 +0200

    Select project

--- client/src/pages/CreatePipe.js
+++ client/src/pages/CreatePipe.js
@@ -1,116 +1,125 @@
 import React, { useState, useEffect } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
 import '../App.css';
 
 const CreatePipe = () => {
+  const [options, setOptions] = useState([]);
+  const [selectedOption, setSelectedOption] = useState('');
+  const [pipelineName, setPipelineName] = useState('');
+  const [language, setLanguage] = useState('');
+  const [actions, setActions] = useState('');
 
-//setup form
-const [options, setOptions] = useState([]);
-const [selectedOption, setSelectedOption] = useState('');
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/projects/myProjects', {
+        headers: {
+          accessToken: localStorage.getItem('accessToken'),
+        },
+      })
+      .then((response) => {
+        setOptions(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
-/*useEffect(() => {
-  // Simulating an API call to retrieve data from the database
-  const fetchData = async () => {
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const initialValuesPN = {
+    pipelineName: '',
+    language: '',
+    actions: '',
+    stateOfPipeline: 'not started',
+    ProjectId: '',
+  };
+
+  const validationSchemaPN = Yup.object().shape({
+    pipelineName: Yup.string().required(),
+    language: Yup.string().required(),
+    actions: Yup.string().required(),
+    stateOfPipeline: Yup.string().required(),
+    ProjectId: Yup.number().required(),
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      // Make the API call to fetch the data from the database
-      const response = await fetch('API_ENDPOINT');
-      const data = await response.json();
-      // Assuming the response is an array of objects with `id` and `name` properties
-      setOptions(data);
+      const dataToSend = {
+        pipelineName,
+        language,
+        actions,
+        stateOfPipeline: 'not started',
+        ProjectId: selectedOption,
+      };
+      const response = await axios.post(
+        'http://localhost:3001/dashboard/createPipeline',
+        dataToSend,
+        {
+          headers: {
+            accessToken: localStorage.getItem('accessToken'),
+          },
+        }
+      );
+      if (response.data.error) {
+        alert(response.data.error);
+      } else {
+        alert('Successfully created!');
+      }
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error('Error submitting form:', error);
     }
   };
 
-  fetchData();
-}, []);*/
-useEffect(() => {
-  axios.get("http://localhost:3001/projects/myProjects", {
-    headers: {
-        accessToken: localStorage.getItem("accessToken"),
-    }
-}).then((response) =>{
-  setOptions(response.data);
-  })
-}, []);
+  return (
+    <div className="create-pipe-container">
+      <h2>Create Pipe</h2>
+      <form className="create-pipe-form" onSubmit={handleSubmit}>
+        <label>Pipe Name:</label>
+        <input
+          type="text"
+          name="pipelineName"
+          placeholder="Pipe Name"
+          value={pipelineName}
+          onChange={(e) => setPipelineName(e.target.value)}
+        />
 
-const handleOptionChange = (event) => {
-  setSelectedOption(event.target.value);
-};
+        <label>Language:</label>
+        <input
+          type="text"
+          name="language"
+          placeholder="Language"
+          value={language}
+          onChange={(e) => setLanguage(e.target.value)}
+        />
 
-const initialValuesPN = {
-  pipelineName: '',
-  language: '',
-  actions: '',
-  stateOfPipeline: 'not started',
-  ProjectId: null,
+        <label>Actions:</label>
+        <input
+          type="text"
+          name="actions"
+          placeholder="Actions"
+          value={actions}
+          onChange={(e) => setActions(e.target.value)}
+        />
 
-};
+        <select name="ProjectId" onChange={handleOptionChange}>
+          <option value="">Select a project</option>
+          {options.map((option) => (
+            <option key={option.id} value={option.id}>
+              {option.projectName}
+            </option>
+          ))}
+        </select>
 
-const validationSchemaPN = Yup.object().shape({
-  pipelineName: Yup.string().required(),
-  language: Yup.string().required(),
-  actions: Yup.string().required(),
-  stateOfPipeline: Yup.string().required(),
-  ProjectId: Yup.number().required()
-
-});
-
-const onSubmit = (data) => {
-  data.ProjectId = selectedOption;
-  axios.post('http://localhost:3001/dashboard/createPipeline', data, {
-      headers: {
-          accessToken: localStorage.getItem("accessToken"),
-      }
-  })
-  .then((response) => {
-    if (response.data.error) {
-      alert(response.data.error);
-    } else {
-    alert("Successfully created!");
-    }
-  },
+        <button type="submit" className="create-pipe-button">
+          ADD PIPE
+        </button>
+      </form>
+    </div>
   );
 };
 
-
-  return (
-    <div className="create-pipe-container">
-        <h2>Create Pipe</h2>
-        <Formik initialValues={initialValuesPN} onSubmit={onSubmit} validationSchema={validationSchemaPN}>
-        <Form className="create-pipe-form">
-
-            <label>Pipe Name:</label>
-            <Field id="pipelineName" name="pipelineName" placeholder="Pipe Name"/>
-          
-
-            <label>Language:</label>
-            <Field id="language" name="language" placeholder="Language"/>
-            
-
-            <label> Actions: </label>
-            <Field id="actions" name="actions" placeholder="Actions"/>
-
-
-           
-          <Field as="select" name="selectedOption" >
-            <option value="">Select a project</option >
-            {options.map((option) => (
-              <option key={option.id} value={option.id} >
-                {option.projectName}
-              </option>
-            ))}
-          </Field>
-       
-        <p>Selected Option: {selectedOption}</p>
-            <button type="submit" className="create-pipe-button">
-            ADD PIPE
-          </button>
-        </Form>
-        </Formik>
-    </div>
-  )
-}
-export default CreatePipe
+export default CreatePipe;
